78f8095927ca15a05bba35cbff0f2be08cf816a7
saqib-dev <developermsaqib@gmail.com>
2024-05-27 15:34:18 +0500

    Sidebar section Completed

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { io, getReceiverSocketId } from "../socket/socket.js";
 export const sendMessage = async (req, res) => {
   try {
     const { message } = req.body;
@@ -26,13 +27,19 @@ export const sendMessage = async (req, res) => {
       conversation.messages.push(newMessage._id);
     }
 
-    // SOCKET IO FUNCTIONALITY WILL HERE
-
     // await conversation.save();
     // await newMessage.save();
     // this will parallel executed
     await Promise.all([conversation.save(), newMessage.save()]);
 
+    // SOCKET IO FUNCTIONALITY WILL HERE
+
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    // io.emit("newMessage", newMessage);
+    if (receiverSocketId) {
+      console.log(receiverSocketId);
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
     res.status(201).json({ status: true, newMessage });
   } catch (error) {
     console.log("Error in SendMessage Controller", error.message);
--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
-
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
+app.use("/api/", messageRoutes);
 app.use("/api/users", userRoutes);
 // app.get("/", (req, res)=>{
 // res.send("Server is Ready")
 // })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(
     `Server is running on port ${PORT} and the link is http://localhost:${PORT}/`
